564a06e458fe19d7bfd1214ddea6b12cc1cf49ce
Tyler Swindell <tyler.swindell.design@gmail.com>
2021-07-11 17:51:02 -0400

    Too many changes to count, oops

--- server.js
+++ server.js
@@ -3,21 +3,26 @@ if (process.env.NODE_ENV !== 'production') require('dotenv').config(); // video 
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
+const authorsRouter = require('./routes/authors');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', err => console.log(err));
 db.on('open', () => console.log('Connected to Mongoose'));
 
 app.use('/', indexRouter);
+app.use('/authors', authorsRouter);
 
 app.listen(process.env.PORT || 3000, () => (process.env.PORT) ? console.log(`Server Started:${process.env.PORT}`) : console.log(`Server Started:3000`));
